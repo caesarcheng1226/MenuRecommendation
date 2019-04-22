b6ffd68490ae56024829d2db13236e116492d470
caesarcheng1226 <caesarcheng1226@gmail.com>
2019-04-23 00:56:06 +0800

    first commit

--- app/src/main/java/com/example/menurecommendation/MainActivity.java
+++ app/src/main/java/com/example/menurecommendation/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.menurecommendation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.profile);
+
+
+
+
     }
 }
